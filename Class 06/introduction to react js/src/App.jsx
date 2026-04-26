8119b2e63ca1ce7763465c48f7021a95eec6cef4
sarimbaig08 <sarimbaig321@gmail.com>
2026-04-26 16:44:33 +0500

    intro to react js

---
+++ Class 06/introduction to react js/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer/Footer.jsx";
+import { Footer2 } from "./components/Footer/footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+      <Footer2 />
+    </>
+  );
+}
+
+export default App;
---
+++ Class 06/introduction to react js/src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright &copy; 2024</p>
+    </footer>
+  );
+}
+
+function Footer2() {
+  return (
+    <footer>
+      <p>Copyright &copy; 2024</p>
+    </footer>
+  );
+}
+
+function Footer3() {
+  return (
+    <footer>
+      <p>Copyright &copy; 2024</p>
+    </footer>
+  );
+}
+
+export default Footer;
+export { Footer2, Footer3 };
